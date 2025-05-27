46311bc458f9d17255794a263435c9280d6ba26c
Geraud Kameni <geraudkameni@gmail.om>
2025-05-26 21:57:28 -0500

    added response

--- app/main.go
+++ app/main.go
@@ -4,13 +4,17 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+var response string
 
 func main() {
+	response := ""
+
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
@@ -27,7 +31,26 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	response := "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n"
+
+	buf := make([]byte, 1024)
+
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from connection:", err)
+	}
+	//fmt.Println(string(buf[:n]))
+	// Parse request line
+
+	requestLine := strings.Split(string(buf[:n]), "\r\n")[0]
+	parts := strings.Split(requestLine, " ")
+
+	if len(parts) >= 2 && parts[1] == "/" {
+		response = "HTTP/1.1 200 OK\r\n\r\n"
+	} else {
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+
+	//response := "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n"
 	conn.Write([]byte(response))
 	conn.Close()
 }
